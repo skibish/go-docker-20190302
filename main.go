3c27f642d18e44aabe242497f233d07d6961be52
Sergey Kibish <sergey.kibish@gmail.com>
2019-03-02 13:00:35 +0200

    modify handler to be more dynamic

--- main.go
+++ main.go
@@ -3,10 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
+var atHome bool // false
+var num int     // 0
+var str string  // ""
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world")
+	parts := strings.Split(r.URL.Path[1:], "/")
+
+	text := parts[1]
+	if text == "" {
+		w.WriteHeader(404)
+		fmt.Fprint(w, "Failed")
+		return
+	}
+
+	fmt.Fprintf(w, "Hello, %s", text)
 }
 
 func main() {
